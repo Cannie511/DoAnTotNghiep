757af6c5d7be3e31fdc97ada669bd4e6be43ab82
Thanh Nam <ngtnam111@example.com>
2024-06-30 14:30:18 +0700

    da lam phan tim kiem ban be, gui loi moi (chua socket), xoa ket ban, show danh ban be

--- src/migrations/20240605145235-create-user-friend.js
+++ src/migrations/20240605145235-create-user-friend.js
@@ -29,6 +29,10 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
       },
+      status: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
